4bafdbaec52d1596c9c80ea06bcb819535378c16
dom111 <dom111@users.noreply.github.com>
2021-02-18 17:19:44 +0000

    Ensure `registerRules` is called.

--- registerRules.js
+++ registerRules.js
@@ -2,6 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const RuleRegistry_1 = require("@civ-clone/core-rule/RuleRegistry");
 const year_1 = require("./Rules/Turn/year");
-RuleRegistry_1.instance
-    .register(...year_1.default());
+RuleRegistry_1.instance.register(...year_1.default());
 //# sourceMappingURL=registerRules.js.map
